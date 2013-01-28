cd7ebb471747c9e5808313765ca43f63c9e18ee0
rubybrahpc <rubybrahpc@gmail.com>
2013-01-28 14:39:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bewerbungsclip28012013::Application.config.secret_token = 'f5d69d0c79a4cfc3f7772b4cfb77cb5c5ff88e276db1231c420d3d79205d0679d5931a99ab417f3196d37a8eef3388a81658700f5bb56cbb8c94fd0031337bc5'
