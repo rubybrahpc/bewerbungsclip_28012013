02d5728cca1eaf61bac04914e877efb965ade1cb
rubybrahpc <rubybrahpc@gmail.com>
2013-01-28 19:23:57 +0100

    Finish Signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :vorname, :nachname, :name, :email, :password, :password_confirmation
+  attr_accessible :vorname, :nachname, :email, :password, :password_confirmation
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :vorname, presence: true, length: { maximum: 25 }
   validates :nachname, presence: true, length: { maximum: 25 }
@@ -13,5 +14,11 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  private
+  
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Bewerbungsclip28012013::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match "/signup", to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
     
